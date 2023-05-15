7482e7a800425509dbd675a2e3bd6d325615261f
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-05-15 12:54:06 +0200

    Created navbar and footer components

--- thub-front/src/app/app.module.ts
+++ thub-front/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemecompComponent } from './memecomp/memecomp.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemecompComponent
+    MemecompComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
