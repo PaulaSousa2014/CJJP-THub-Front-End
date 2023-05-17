6a862739424dc3bdbec67b2e2cb2dc4923282fcd
JPrimee <javierpsantos99@gmail.com>
2023-05-17 13:23:43 +0200

    chat phase 1

--- thub-front/src/app/chat/chat.component.ts
+++ thub-front/src/app/chat/chat.component.ts
@@ -7,10 +7,27 @@ import { Component } from '@angular/core';
 })
 export class ChatComponent {
 
-  activeTab = 'tab1';
+  selectedTab = 'parties';
   changeTab(tabId: string) {
-    // Change private messages tab and parties tab
-    this.activeTab = tabId;
+    this.selectedTab = tabId;
+  }
+
+  contactos: { id: number, nombre: string }[] = [
+    { id: 1, nombre: 'Juan Pérez' },
+    { id: 2, nombre: 'María García' },
+    { id: 3, nombre: 'Pedro López' },
+    { id: 4, nombre: 'Ana Rodríguez' },
+    { id: 5, nombre: 'Luis Martínez' },
+    { id: 6, nombre: 'Laura Fernández' },
+    { id: 7, nombre: 'Carlos González' },
+    { id: 8, nombre: 'Sofía Ramírez' },
+    { id: 9, nombre: 'Miguel Torres' },
+    { id: 10, nombre: 'Isabel Sánchez' }
+  ];
+
+  usuarioSeleccionado: string | null = null;
+  seleccionarUsuario(usuario: string) {
+    this.usuarioSeleccionado = usuario;
   }
 
 }
