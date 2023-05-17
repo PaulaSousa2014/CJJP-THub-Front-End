b2f5468ccc8eba9227622aa75e9df39ade0cf99a
camilagarcia36 <garciiaperezz@gmail.com>
2023-05-17 12:49:59 +0200

    create parties component

--- thub-front/src/app/app.module.ts
+++ thub-front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MemecompComponent } from './memecomp/memecomp.component';
 import { LoginComponent } from './login/login.component';
+import { PartiesComponent } from './parties/parties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MemecompComponent,
-    LoginComponent
+    LoginComponent,
+    PartiesComponent
   ],
   imports: [
     BrowserModule,
