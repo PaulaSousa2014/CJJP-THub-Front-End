6a6c8d8ef17a7c90c2fa125e2e2d134ad14fdf0a
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-05-18 18:06:34 +0200

    Code structuring

--- thub-front/src/app/app.module.ts
+++ thub-front/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MemecompComponent } from './memecomp/memecomp.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { MemecompComponent } from './components/memecomp/memecomp.component';
+import { NavbarComponent } from './components/navbars/navbar/navbar.component';
+import { MininavbarComponent } from './components/navbars/mininavbar/mininavbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainFeedComponent } from './components/main-feed/main-feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MemecompComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MainFeedComponent,
+    MininavbarComponent
   ],
   imports: [
     BrowserModule,
