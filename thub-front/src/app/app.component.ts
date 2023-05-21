ef458366914fef44a15d1a24b1554b641d2968c9
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-05-21 18:59:27 +0200

    Navbar adjustments

--- thub-front/src/app/app.component.ts
+++ thub-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,22 @@ export class AppComponent {
   title = 'thub-front';
   /* Boolean is logged */
   isLogged: boolean = true;
+  /* Show navbar */
+  showNavbar: boolean = true;
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Update showNavBar depending on atual route
+        this.showNavbar = !this.isSpecificRoute(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  isSpecificRoute(url: string): boolean {
+    // Define the routes
+    const hiddenRoutes = ['/login', '/signup'];
+    // Compare with the actual route
+    return hiddenRoutes.some(route => url.startsWith(route));
+  }
 }
