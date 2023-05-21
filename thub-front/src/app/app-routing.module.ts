e5f103ab3495d651950a0ae304b317edb84dd37b
Camila Garcia <garciiaperezz@gmail.com>
2023-05-21 12:13:35 +0200

    cambios errrores routin

--- thub-front/src/app/app-routing.module.ts
+++ thub-front/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { LoginComponent } from './components/login/login.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { MainFeedComponent } from './components/main-feed/main-feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/landingpage',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landingpage',
+    component: LandingComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: MainFeedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
