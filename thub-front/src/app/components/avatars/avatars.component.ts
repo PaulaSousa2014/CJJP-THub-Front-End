5cd36a4f0fd087a8420a753ce8f1125ff6fb4b9c
Paula Sousa <paulasousa137@gmail.com>
2023-05-22 19:59:56 +0200

    avatar selection

--- thub-front/src/app/components/avatars/avatars.component.ts
+++ thub-front/src/app/components/avatars/avatars.component.ts
@@ -8,6 +8,13 @@ import { Component } from '@angular/core';
 export class AvatarsComponent {
 
   avatars: string[] = ['cat0.jpg', 'cat1.jpg', 'cat2.jpg', 'cat3.jpg', 'cat4.jpg', 'cat5.jpg', 'cat6.jpg',
-  'cat7.jpg', 'cat8.jpg', 'cat9.jpg', 'cat10.jpg']
+  'cat7.jpg', 'cat8.jpg', 'cat9.jpg', 'cat10.jpg'];
+  selectedAvatar: string = '';
+  savedAvatar: string = '';
+
+  selectAvatar(image: string): void {
+    this.selectedAvatar = image;
+  }
+
 
 }
