cc709fe2a49d584dff13d1e47915f2ca8a81e74e
Josep Mª Pallàs Batalla <jpb_montblanc@hotmail.com>
2023-05-23 09:39:25 +0200

    Looks horrible, but login gets token

--- thub-front/src/app/components/login/login.component.ts
+++ thub-front/src/app/components/login/login.component.ts
@@ -1,9 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  form: any = {
+    username: "",
+    password: ""
+  };
+
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = "";
+  roles: string[] = [];
+
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+
+  ngOnInit(): void {
+    console.log("welcome");
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { username, password } = this.form;
+
+    console.log(username, password);
+    this.authService.login(username, password).subscribe({
+      next: (data: any) => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        this.reloadPage();
+      },
+      error: (err: any) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
 
 }
