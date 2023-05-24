b79aa8525d1d7fd51a17674e84e4d859dd15cdcb
Paula Sousa <paulasousa137@gmail.com>
2023-05-24 20:31:58 +0200

    get profile info from db

--- thub-front/src/app/components/editprofile/editprofile.component.ts
+++ thub-front/src/app/components/editprofile/editprofile.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { JobService } from 'src/app/services/job.service';
+import { OfficesService } from 'src/app/services/offices.service';
 
 @Component({
   selector: 'app-editprofile',
   templateUrl: './editprofile.component.html',
-  styleUrls: ['./editprofile.component.css']
+  styleUrls: ['./editprofile.component.css'],
 })
 export class EditprofileComponent {
+  userProfile: any;
+  user: any;
+  jobOptions: any[] = [];
+  officeOptions: any[] = [];
 
+  constructor(
+    private tokenStorageService: TokenStorageService,
+    private userService: UserService,
+    private jobService: JobService,
+    private officeService: OfficesService
+  ) {}
+
+  ngOnInit() {
+    this.user = this.tokenStorageService.getUser();
+    this.getUserProfile();
+  }
+
+  getUserProfile() {
+    this.userService.getUser(this.user.id).subscribe({
+      next: (profile: any) => {
+        this.userProfile = profile;
+        this.getProfileData();
+        this.getJobOptions();
+        this.getOfficeOptions();
+        console.log(this.userProfile);
+      },
+      error: (error) => {
+        console.log('Something went wrong', error);
+      },
+    });
+  }
+
+  getProfileData() {
+    if (this.userProfile.nameSurn) {
+      this.user.nameSurn = this.userProfile.nameSurn;
+    }
+    if (this.userProfile.email) {
+      this.user.email = this.userProfile.email;
+    }
+    if (this.userProfile.steam_username) {
+      this.user.steam_username = this.userProfile.steam_username;
+    }
+    if (this.userProfile.username) {
+      this.user.username = this.userProfile.username;
+    }
+  }
+
+  getJobOptions() {
+    this.jobService.getJobs().subscribe({
+      next: (jobs: any[]) => {
+        this.jobOptions = jobs.map(job => ({
+          id: job.id,
+          title: job.title,
+          selected: job.title === this.userProfile.job?.title 
+        }));
+      },
+      error: (error) => {
+        console.log('Something went wrong', error);
+      },
+    });
+  }
+
+  getOfficeOptions(){
+    this.officeService.getOffices().subscribe({
+      next: (offices: any[]) => {
+        this.officeOptions = offices.map(office => ({
+          id: office.id,
+          name: office.name,
+          selected: office.name === this.userProfile.office?.name 
+        }));
+      },
+      error: (error) => {
+        console.log('Something went wrong', error);
+      },
+    });
+
+  }
+   
 }
