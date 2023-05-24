a4988b9d3fa03a72a15e1a8feb9928b98bdecc15
JPrimee <javierpsantos99@gmail.com>
2023-05-24 10:14:19 +0200

    Funciona colocando una id

--- thub-front/src/app/components/profile/profile.component.ts
+++ thub-front/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  // User array
+  user: any;
 
+  constructor(private userService: UserService, private tokenStorage: TokenStorageService) {}
+  id: number = 3;
+
+  ngOnInit() {
+    this.getUserById();
+    //this.user = this.tokenStorage.getUser();
+  }
+
+  //Function on get user by id
+  getUserById() {
+    this.userService.getUser(this.id).subscribe({
+      next: (data: any) => {
+        this.user = data;
+        console.log(this.user);
+      },
+      error: (error: any) => {
+        console.log("Cannot get user", error);
+      }
+    });
+  }
 }
