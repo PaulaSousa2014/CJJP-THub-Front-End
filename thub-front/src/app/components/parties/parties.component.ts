95b2e211ac1afdcb95f3c307eccd08cab05a05a1
camilagarcia36 <garciiaperezz@gmail.com>
2023-05-24 11:06:27 +0200

    parties by user done

--- thub-front/src/app/components/parties/parties.component.ts
+++ thub-front/src/app/components/parties/parties.component.ts
@@ -1,5 +1,8 @@
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 import { Component } from '@angular/core';
 import { PartiesService } from 'src/app/services/parties.service';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-parties',
@@ -8,20 +11,28 @@ import { PartiesService } from 'src/app/services/parties.service';
 })
 export class PartiesComponent {
   parties: any[] = [];
-  filteredParties: any[] = []; // Nuevo arreglo para almacenar las parties filtradas
-  selectedPartyType: string = ''; // Variable para almacenar el tipo de party seleccionado
+  filteredParties: any[] = [];
+  selectedPartyType: string = '';
+  user: any = this.tokenStorageService.getUser();
+  userId = this.user.id;
 
-  constructor(private partiesService: PartiesService) {}
+  constructor(private partiesService: PartiesService, private authService: AuthService, private tokenStorageService: TokenStorageService) {
+  }
 
   ngOnInit() {
     this.getAllParties();
   }
 
-  getAllParties() {
+  getAllParties(filterByUser: boolean = false) {
     this.partiesService.getParties().subscribe({
       next: (data: any) => {
         this.parties = data;
-        this.filteredParties = this.parties; // Inicialmente, mostrar todas las parties sin filtrar
+        this.filteredParties = this.parties;
+
+        if (filterByUser) {
+          this.filteredParties = this.parties.filter(party => party.creator && party.creator.id === this.userId);
+        }
+
         console.log(this.parties);
       },
       error: (error: any) => {
@@ -34,9 +45,15 @@ export class PartiesComponent {
     this.selectedPartyType = partyType;
 
     if (partyType === 'all') {
-      this.filteredParties = this.parties; // Mostrar todas las parties sin filtrar
+      this.filteredParties = this.parties;
     } else {
-      this.filteredParties = this.parties.filter(party => party[partyType] !== null); // Filtrar las parties según el tipo seleccionado
+      this.filteredParties = this.parties.filter(party => party[partyType] !== null);
     }
   }
+
+  filterMyParties() {
+    this.filteredParties = this.parties.filter(party => party.creator.id === this.userId);
+  }
+
+
 }
