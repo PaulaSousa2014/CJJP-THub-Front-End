cdc30def0dfc6e7c376c68feadf69bf156257130
Paula Sousa <paulasousa137@gmail.com>
2023-05-24 10:41:12 +0200

    navbar logout

--- thub-front/src/app/components/navbars/navbar/navbar.component.ts
+++ thub-front/src/app/components/navbars/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  isLoggedIn = true;
 
+  constructor(
+    private tokenStorage: TokenStorageService,
+    private router: Router
+  ) {}
+
+  logOut(): void {
+    console.log('logout function');
+    this.tokenStorage.signOut();
+    this.reloadPage();
+  }
+
+  reloadPage(): void {
+    this.router.navigate(['landingpage']);
+  }
 }
