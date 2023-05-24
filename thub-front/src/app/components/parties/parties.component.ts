ed098290177dc7b33066e4a59eaf76fd752509ed
camilagarcia36 <garciiaperezz@gmail.com>
2023-05-24 10:22:41 +0200

    parties getting title and description bbdd

--- thub-front/src/app/components/parties/parties.component.ts
+++ thub-front/src/app/components/parties/parties.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PartiesService } from 'src/app/services/parties.service';
 
 @Component({
   selector: 'app-parties',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./parties.component.css']
 })
 export class PartiesComponent {
+  //array parties
+  parties: any[] = [];
+
+  constructor(private partiesService: PartiesService) {}
+
+  // On page load, get all posts
+  ngOnInit() {
+    this.getAllParties();
+  }
+
+  // Function to get posts and get likes/comments
+  getAllParties() {
+    this.partiesService.getParties().subscribe({
+      next: (data: any) => {
+        this.parties = data;
+        console.log(this.parties);
+      },
+      error: (error: any) => {
+        console.log("Cannot get parties", error);
+      }
+    });
+  }
 
 }
