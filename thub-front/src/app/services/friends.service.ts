871f7af45a04e4e43056592f2c9b11ea10eea78e
JPrimee <javierpsantos99@gmail.com>
2023-05-25 09:41:04 +0200

    Friends XII: Optimization Part I

--- thub-front/src/app/services/friends.service.ts
+++ thub-front/src/app/services/friends.service.ts
@@ -18,4 +18,35 @@ export class FriendsService {
     return this.httpClient.get(USER_API, httpOptions);
   }
 
+  /* Get all fiends list */
+  getFriendsList(id: number) {
+    let friends: any[] = [];
+
+    this.getFriends().subscribe({
+      next: (data: any) => {
+        friends = data.filter((friend: any) => friend.userSender.id === id || friend.userReciever.id === id);
+        this.getMyFriends(friends, id);
+      },
+      error: (error: any) => {
+        console.log("Cannot get friend", error);
+      }
+    });
+  }
+
+  /* Get friends by id */
+  getMyFriends(friends: any[], id: number) {
+    friends = friends.map((friend: any) => {
+      if (friend.userSender.id === id) {
+        const tempReceiver = { ...friend.userReciever };
+
+        return {
+          ...friend,
+          userSender: { ...tempReceiver },
+          userReciever: { ...friend.userSender }
+        };
+      } else {
+        return friend;
+      }
+    });
+  }
 }
