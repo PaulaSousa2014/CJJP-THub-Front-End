ccdcd7a11b7c3613d99499838bf7a4d895d654d2
JPrimee <javierpsantos99@gmail.com>
2023-05-25 17:48:41 +0200

    FXXII: Finally works!

--- thub-front/src/app/components/profile/profile.component.ts
+++ thub-front/src/app/components/profile/profile.component.ts
@@ -10,15 +10,15 @@ import { FriendsService } from 'src/app/services/friends.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  myProfile: boolean = true;
-  showData: number = 0;
+  myProfile = true;
+  showData = 0;
   user: any;
   friends: any[] = [];
   friendsRS: any[] = [];
   friendsRR: any[] = [];
 
-  button: number = 0;
-  buttonChanged: boolean = false;
+  button = 0;
+  buttonChanged = false;
 
   constructor(
     private userService: UserService,
@@ -30,57 +30,14 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       const showDataParam = params.get('showData');
-      if (showDataParam !== null) {
-        this.showData = parseInt(showDataParam);
-      }
+      this.showData = showDataParam !== null ? parseInt(showDataParam) : 0;
     });
 
     this.user = this.tokenStorage.getUser();
-    if (this.showData !== this.user.id) {
-      this.myProfile = false;
-    }
+    this.myProfile = this.showData === this.user.id;
 
     this.getUserById(this.showData);
-
-    this.friendsService.getFriendsListTestFormat(this.tokenStorage.getUser().id, true).subscribe(
-      (friends: any[]) => {
-        this.friends = friends;
-        this.checkFriends();
-
-        if (this.buttonChanged) {
-          console.log("El botón ha cambiado a " + this.button);
-        } else {
-          console.log("El botón no ha cambiado");
-        }
-      },
-      (error: any) => {
-        console.log("Error retrieving friends list", error);
-      }
-    );
-
-    this.friendsService.getFriendsList(this.tokenStorage.getUser().id, false, true).subscribe(
-      (friends: any[]) => {
-        this.friendsRS = friends;
-        console.log("RS" + this.showData);
-        console.log(this.friendsRS);
-        console.log("La ID del primer registro es: " + this.friendsRS[0].userSender.id);
-        this.checkFriends();
-      },
-      (error: any) => {
-        console.log("Error retrieving friends list", error);
-      }
-    );
-
-    this.friendsService.getFriendsListTest(this.tokenStorage.getUser().id).subscribe(
-      (friends: any[]) => {
-        this.friendsRR = friends;
-        console.log(this.friendsRR);
-        this.checkFriends();
-      },
-      (error: any) => {
-        console.log("Error retrieving friends list", error);
-      }
-    );
+    this.getFriendsLists();
   }
 
   getUserById(id: number) {
@@ -95,28 +52,64 @@ export class ProfileComponent implements OnInit {
     });
   }
 
-  checkFriends() {
-    let newButton = 0;
-
-    if (this.friends.some(friend => friend.userSender.id === this.showData && friend.status === true)) {
-      newButton = 1; // Friends
-    } else {
-      if (this.friendsRS.some(friend => friend.userSender.id === this.showData)) {
-        newButton = 2; // Accept
-      } else {
-        if (this.friendsRR.some(friend => friend.userReciever.id === this.showData)) {
-          newButton = 3; // Pending
-        } else {
-          newButton = 0; // No friends
-        }
+  getFriendsLists() {
+    this.friendsService.getFriendsListTestFormat(this.tokenStorage.getUser().id, true).subscribe(
+      (friends: any[]) => {
+        this.friends = friends;
+        this.checkFriends();
+        this.printButtonStatus();
+      },
+      (error: any) => {
+        console.log("Error retrieving friends list", error);
       }
+    );
+
+    this.friendsService.getFriendsList(this.tokenStorage.getUser().id, false, true).subscribe(
+      (friends: any[]) => {
+        this.friendsRS = friends;
+        console.log("RS" + this.showData);
+        console.log(this.friendsRS);
+        console.log("La ID del primer registro es: " + (this.friendsRS[0]?.userSender?.id || 'N/A'));
+        this.checkFriends();
+        this.printButtonStatus();
+      },
+      (error: any) => {
+        console.log("Error retrieving friends list", error);
+      }
+    );
+
+    this.friendsService.getFriendsListTest(this.tokenStorage.getUser().id).subscribe(
+      (friends: any[]) => {
+        this.friendsRR = friends;
+        console.log(this.friendsRR);
+        this.checkFriends();
+        this.printButtonStatus();
+      },
+      (error: any) => {
+        console.log("Error retrieving friends list", error);
+      }
+    );
+  }
+
+  checkFriends() {
+    if (this.friends.some(friend => friend.userSender.id === this.showData && friend.status === true)) {
+      this.button = 1; // Friends
+    } else if (this.friendsRS.some(friend => friend.userSender.id === this.showData)) {
+      this.button = 2; // Accept
+    } else if (this.friendsRR.some(friend => friend.userReciever.id === this.showData)) {
+      this.button = 3; // Pending
+    } else {
+      this.button = 0; // No friends
     }
 
-    if (newButton !== this.button) {
-      this.button = newButton;
-      this.buttonChanged = true;
+    this.buttonChanged = this.button !== this.button;
+  }
+
+  printButtonStatus() {
+    if (this.buttonChanged) {
+      console.log("El botón ha cambiado a " + this.button);
     } else {
-      this.buttonChanged = false;
+      console.log("El botón no ha cambiado");
     }
   }
 }
