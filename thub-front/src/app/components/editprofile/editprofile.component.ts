b5ef3fdac74947638920122f7244b76e44240aab
Paula Sousa <paulasousa137@gmail.com>
2023-05-25 14:02:38 +0200

    edit profile finished

--- thub-front/src/app/components/editprofile/editprofile.component.ts
+++ thub-front/src/app/components/editprofile/editprofile.component.ts
@@ -12,17 +12,15 @@ import { AvatarService } from 'src/app/services/avatar.service';
   styleUrls: ['./editprofile.component.css'],
 })
 export class EditprofileComponent {
+
   userProfile: any;
   user: any;
   jobOptions: any[] = [];
   officeOptions: any[] = [];
-  updatedUser: any ={};
-  job: Job = {} as Job;
-  office: Office = {} as Office;
-  selectedJob: any=0;
-  selectedOffice: any=0;
+  updatedUser: any = {};
+  selectedJob: any = 0;
+  selectedOffice: any = 0;
   image: any;
-
 
   constructor(
     private tokenStorageService: TokenStorageService,
@@ -30,59 +28,54 @@ export class EditprofileComponent {
     private jobService: JobService,
     private officeService: OfficesService,
     private avatarService: AvatarService
-  ) { }
+  ) {}
 
   //Inicializing with user data and avatar image
   ngOnInit() {
+
     this.user = this.tokenStorageService.getUser();
     this.image = this.avatarService.getSelectedAvatar();
     this.getUserProfile();
-
   }
 
   //Get profile data from user db
   getUserProfile() {
-  this.userService.getUser(this.user.id).subscribe({
+
+    this.userService.getUser(this.user.id).subscribe({
+
       next: (profile: any) => {
-        this.userProfile = profile; 
-        this.getProfileData(); //Get inputs data
+
+        this.userProfile = profile;
+        this.user.nameSurn = this.userProfile.nameSurn;
+        this.user.profile_image = this.userProfile.profile_image;
+        this.user.roles = this.userProfile.roles;
         this.getJobOptions(); // Get job data
         this.getOfficeOptions(); //Get office data
       },
       error: (error) => {
+
         console.log('Something went wrong', error);
       },
     });
   }
 
-  getProfileData() {
-    if (this.userProfile.nameSurn) {
-      this.user.nameSurn = this.userProfile.nameSurn;
-    }
-    if (this.userProfile.email) {
-      this.user.email = this.userProfile.email;
-    }
-    if (this.userProfile.steam_username) {
-      this.user.steam_username = this.userProfile.steam_username;
-    }
-    if (this.userProfile.username) {
-      this.user.username = this.userProfile.username;
-    }
-   
-
-  }
-  
   getJobOptions() {
+
     this.jobService.getJobs().subscribe({
+
       next: (jobs: any[]) => {
+
         this.jobOptions = jobs.map((job) => ({
           id: job.id,
           title: job.title,
-          description: job.description
+          description: job.description,
         }));
 
         // Set the default selected job
-        this.selectedJob = this.jobOptions.find(option => option.title === this.userProfile.job?.title) || 0;
+        this.selectedJob =
+          this.jobOptions.find(
+            (option) => option.title === this.userProfile.job?.title
+          ) || 0;
       },
       error: (error) => {
         console.log('Something went wrong', error);
@@ -91,16 +84,22 @@ export class EditprofileComponent {
   }
 
   getOfficeOptions() {
+
     this.officeService.getOffices().subscribe({
+
       next: (offices: any[]) => {
+
         this.officeOptions = offices.map((office) => ({
           id: office.id,
           name: office.name,
-          location: office.location
+          location: office.location,
         }));
 
         // Set the default selected office
-        this.selectedOffice = this.officeOptions.find(option => option.name === this.userProfile.office?.name) || 0;
+        this.selectedOffice =
+          this.officeOptions.find(
+            (option) => option.name === this.userProfile.office?.name
+          ) || 0;
       },
       error: (error) => {
         console.log('Something went wrong', error);
@@ -108,49 +107,54 @@ export class EditprofileComponent {
     });
   }
 
+  //Update user information
+  updateUser() {
+    
+    this.updatedUser = {
 
-//Save changed info
-save(): void {
-  console.log('dentro de save');
+      username: this.userProfile.username,
+      password: this.userProfile.password,
+      email: this.userProfile.email,
+      nameSurn: this.userProfile.nameSurn,
+      profile_image: this.userProfile.profile_image,
+      steam_username: this.userProfile.steam_username,
 
-  // Actualizar los campos del objeto updatedUser
-  this.updatedUser = {
-    ...this.user,
-    job: {
-      id: this.selectedJob.id,
-      title: this.selectedJob.title,
-      description: this.selectedJob.description,
-    },
-    office: {
-      id: this.selectedOffice.id,
-      name: this.selectedOffice.name,
-      location: this.selectedOffice.location,
-    },
-  };
+      job: {
+        id: this.selectedJob.id,
+        title: this.selectedJob.title,
+        description: this.selectedJob.description,
+      },
 
-  console.log(this.updatedUser);
+      office: {
+        id: this.selectedOffice.id,
+        name: this.selectedOffice.name,
+        location: this.selectedOffice.location,
+      },
 
-  // Llamar al servicio de actualización de usuario para enviar los cambios al servidor
-  this.userService.updateUser(this.user.id, this.updatedUser).subscribe({
-    next: (response) => {
-      // Comprobar si la respuesta es válida
-      if (response) {
-        // Notificar que se actualizó correctamente
-        alert('Character updated successfully!');
-      }
-    },
-    error: (error) => {
-      console.log('Something went wrong', error);
-    },
-  });
+      roles: this.userProfile.roles,
+    };
+  }
+
+  //Save changed info
+  save(): void {
+
+    // Update user info
+    this.updateUser();
+
+    console.log(this.updatedUser);
+
+    // Save changes into DB
+    this.userService.updateUser(this.user.id, this.updatedUser).subscribe({
+      next: (response) => {
+        // check is valid info
+        if (response) {
+          // notify if it is OK
+          alert('Character updated successfully!');
+        }
+      },
+      error: (error) => {
+        console.log('Something went wrong', error);
+      },
+    });
+  }
 }
-
-
-
-}
-
-
-
-
-
-
