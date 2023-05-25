0d2c3f39f60089c94f371d0c70dfab6411acdfde
camilagarcia36 <garciiaperezz@gmail.com>
2023-05-25 23:07:25 +0200

    detail party functional

--- thub-front/src/app/components/click-party/click-party.component.ts
+++ thub-front/src/app/components/click-party/click-party.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Creator, Party } from 'src/app/models/PartyModels';
+import { PartiesService } from 'src/app/services/parties.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-click-party',
   templateUrl: './click-party.component.html',
   styleUrls: ['./click-party.component.css']
 })
-export class ClickPartyComponent {
+export class ClickPartyComponent implements OnInit {
+
+   // Attribute to store id and character
+   id: number = 0;
+   party: Party = {} as Party;
+   creator: Creator = { } as Creator;
+   currentUser: any;
+
+  constructor(private partiesService: PartiesService, private route: ActivatedRoute, private router: Router, private tokenStorage: TokenStorageService) { }
+
+
+    ngOnInit(): void {
+      console.log("party inicial: " + this.party);
+      this.currentUser = this.tokenStorage.getUser();
+    this.creator = this.currentUser.creator;
+
+    console.log( "party : "+
+      this.party
+    );
+    console.log( "creator : "+
+      this.creator
+    );
+    console.log( "party : "+
+      this.id
+    );
+      // Get the id from the route parameters
+      this.route.params.subscribe(params => {
+        this.id = +params['id'];
+
+        if (this.id) {
+          this.partiesService.getPartiesId(this.id).subscribe((data: Party) => {
+            console.log("Data: " + data);
+            this.party = data;
+          });
+        }
+
+      });
+
+
+  }
+
+  goBack() {
+    this.router.navigate(['/parties']);
+  }
 
 }
--- thub-front/src/app/components/parties/parties.component.ts
+++ thub-front/src/app/components/parties/parties.component.ts
@@ -2,6 +2,8 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
 import { Component } from '@angular/core';
 import { PartiesService } from 'src/app/services/parties.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { Party } from 'src/app/models/PartyModels';
 
 
 @Component({
@@ -10,36 +12,46 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./parties.component.css']
 })
 export class PartiesComponent {
+
   parties: any[] = [];
   filteredParties: any[] = [];
   selectedPartyType: string = '';
   user: any = this.tokenStorageService.getUser();
   userId = this.user.id;
 
-  constructor(private partiesService: PartiesService, private authService: AuthService, private tokenStorageService: TokenStorageService) {
+
+  constructor(private partiesService: PartiesService, private authService: AuthService, private router: Router, private tokenStorageService: TokenStorageService) {
   }
 
   ngOnInit() {
     this.getAllParties();
- 
   }
 
 
   getAllParties(filterByUser: boolean = false) {
+
     this.partiesService.getParties().subscribe({
       next: (data: any) => {
         this.parties = data;
         this.filteredParties = this.parties.filter(party => party.creator.id === this.userId);
 
         console.log(this.parties);
+
       },
       error: (error: any) => {
-        console.log("Cannot get parties", error);
+        console.log("Cannot get all parties", error);
       }
     });
   }
 
+  readMore(id: number) {
+    this.router.navigate(['/parties/id', id]);
+    console.log(id);
+
+  }
+
   filterPartiesByType(partyType: string) {
+
     this.selectedPartyType = partyType;
 
     if (partyType === 'all') {
@@ -50,6 +62,7 @@ export class PartiesComponent {
   }
 
   filterMyParties() {
+
     this.filteredParties = this.parties.filter(party => party.creator.id === this.userId);
   }
 
@@ -58,4 +71,6 @@ export class PartiesComponent {
   }
 
 
-}
+
+  }
+
