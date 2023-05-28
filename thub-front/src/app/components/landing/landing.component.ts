abf9673ca58c939b784f86c727a7d02da55ffcf0
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-05-28 12:26:53 +0200

    Button carousell fixed

--- thub-front/src/app/components/landing/landing.component.ts
+++ thub-front/src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class LandingComponent {
 
+  constructor(private router: Router) {}
+
+  /* Go to sign up page */
+  navigateToSignup() {
+    this.router.navigate(['/signup']);
+  }
 }
